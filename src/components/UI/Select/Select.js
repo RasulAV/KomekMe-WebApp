16e8e0838ef200aa5703c3cbe46952a7cb620a44
Akhmedkhanov Rasul <rasula@me.com>
2021-12-20 13:02:31 +0400

    delete Banana from select

--- src/components/UI/Select/Select.js
+++ src/components/UI/Select/Select.js
@@ -2,7 +2,6 @@ function Select() {
   return (
     <select>
       <option value="A">Apple</option>
-      <option value="B">Banana</option>
       <option value="C">Cranberry</option>
     </select>
   );
