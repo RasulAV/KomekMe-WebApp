9068b031a5aad1e7c6ab67325e90accde45c6fbc
Akhmedkhanov Rasul <rasula@me.com>
2025-03-16 10:25:14 +0300

    Revert "feat: add i18l to project"
    This reverts commit d76bcce15502f58de38d722e00f6a768a3688d4d.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withNextIntl = require('next-intl/plugin')('./i18n.ts')
+const nextConfig = {}
 
-module.exports = withNextIntl({
-  // Other Next.js config options can go here
-})
+module.exports = nextConfig
